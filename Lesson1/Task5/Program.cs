2cd8b53f3a5220114b4190962117f3722b8eb49e
Vaska_1283 <vasili.gusako@yandex.ru>
2025-05-29 14:28:34 +0300

    lesson1 : task5

---
+++ Lesson1/Task5/Program.cs
@@ -0,0 +1,17 @@
+﻿// Дано натуральное число N. Вывести на экран числа от 1 до N. Например N = 4, -> 1, 2, 3, 4.
+int N = 25;
+int i = 1;
+
+while (i <= N)
+{
+    //Console.WriteLine(i);
+
+    //Console.Write(i);
+    //Console.Write(" ");
+
+    Console.Write($"{i} ");
+
+    //i = i + 1;
+    
+    i++;
+}

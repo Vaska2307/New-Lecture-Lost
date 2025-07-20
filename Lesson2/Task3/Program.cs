87cc063458a5caa35a3e3852a5c79a7646c6652f
Vaska_1283 <vasili.gusako@yandex.ru>
2025-07-20 14:39:41 +0300

    Lesson2 : Task3

---
+++ Lesson2/Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = 10;
+int[] arr = new int[n]; //{2, 5, 4, 7, 8, 35, 1, 3, 12, 9};
+int i = 0;
+while (i < n)
+{
+    arr[i] = i + 1;
+    if (arr[i] % 2 == 0)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    i = i + 1;
+}

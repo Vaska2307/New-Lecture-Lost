579f1e85794b32a58db0283ef5f5e67e34c456a1
Vaska_1283 <vasili.gusako@yandex.ru>
2025-08-03 14:59:40 +0300

    Lesson3 : Task2

---
+++ Lesson3/Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿void PrintSquares(int limit)
+{
+    int i = 1;
+    while (i <= limit)
+    {
+        //int sqr = i * i;
+        Console.Write($"{i * i}, ");
+        i++;
+    }
+    Console.WriteLine();
+}
+
+PrintSquares(5);
+PrintSquares(10);
+PrintSquares(15);
